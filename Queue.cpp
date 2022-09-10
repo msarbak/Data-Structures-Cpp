249c8184c59e4047cdaabb4409794c1188db4a4b
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-09-10 14:06:45 +0300

    Queue

---
+++ Queue.cpp
@@ -0,0 +1,4 @@
+//ilk gelen ilk hizmet gorur, gercek hayat problemlerinde  kullanılan aktif bir veri yapısı
+//hem dizi hemde liste üzerinde gösterecegiz, ekleme cıkarmayı sınırlandıracagız
+#include <iostream>
+using namespace std;
