f506c3378e6c8ffaefced9e3bcaa49c973d95bbf
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-09-11 10:27:19 +0300

    Queue

--- Queue.cpp
+++ Queue.cpp
@@ -25,11 +25,61 @@ int enYuksekBasamakSayisi(int *items, int length) {
 }
 
 int* radixSort(int* items, int length) {
-    Queue<int> **queues = new Queue<int>*[10]; //icinde adres , dizinin kendide adres: çift **
+    Queue<int> **queues = new Queue<int>*[10]; //icinde adres , dizinin kendide adres: çift ** .   kuyruk kdizinin hücresinde deil adresidne olacak
     // 10 tane kuyrugu burada olusturduk
-    
+    //suan icerde anlamsız adresler var , bizim bos kuyrukları olusturmamız lazım
+    for (int i = 0; i < 10; i++){
+        queues[i] = new Queue<int>(); // 10 kuyruk olusuyor
+        int maxBasamak = enYuksekBasamakSayisi(items,length);
+        int sayiIndex = 0;
+        for (int i = 0; i < maxBasamak; i++){ // algoritma icin ana dongumuz, kuyruklara yerlestirme islemi en yüksek basamaga sahip sayının basamak adedince oalcaktır 
+            for (int index = 0; index < 10; index++){
+                if(sayiIndex < length) { //bu da kuyrukların icinde dolasacak 
+                    // kuyruklara ilk ekleme kısmı, ilk ekelem ediziden kuyruklara dogru olur, sonra kuyrukalrın kendi icerisinde basamaklara gore yerlesir
+                    int basamakDegeri = items[sayiIndex] % 10; // sayıyı ona boldum birler basamagını elde ettim
+                    queues[basamakDegeri] -> enqueue(items[sayiIndex++]); //kuyruklar dizisi
+                    //birler absamagındaki deger o degerdeki kuyruga gidiyor, bu sayıyı oraya ekledikten sonrasayıindex 1 artıyor
+                } else{ //birler basmagının degerlendirmesi tamamlanmıs onlar yuzler vs.. burada
+                    for (int len = queues[index]->count(); len > 0; len--){
+                        int sayi = queues[index]->peek(); // sırdaki çıkmak üzere olan sayıyıı alıyoruz
+                        queues[index]->dequeue(); // artık veriyi silebiliriz kaybolmasın diye peek ile almıstık
+                        int basamakDegeri = (sayi/(int)pow(10,i)) % 10; // 10 un üzeri seklinde
+                        queues[basamakDegeri]->enqueue(sayi); // sildigimiz degeri oradaki kuyruga ekleyecegiz
+                    }
+                    
+                }
+            }
+            
+        }
+        
+    }
+    int* sirali = new int [length]; // for dongıuleri bittikten sonra sıralı hale gelmeis oalcak, yeni dizitanımlayıp kullanalım
+    sayiIndex = 0;
+    for (int index = 0; index < 10; index++){ //kuyruklar dizisndeki her sayıyı dolasıcaz ve bu diziye yerlesicez
+        while(!queues[index]->isEmpty()){ //kuyurk bos olmadıgı surece
+            int sayi =  queues[index]->peek(); //cıkartmadan sayiyi elde ediyoruz 
+            sirali[sayiIndex++] = sayi; // bu sayıyı sıralı dizinin icine kopaylıoruz, kopyaladıktan sorna indexi 1 artıyıoruz
+            queues[index]->dequeue(); // kopayladıktan sonra kuyruktan sayısyı silebiliriz
+        }
+    }//cop olamamsı adına new le olusan her kuyrugu simdi silmemeiz lazım
+    for (int i = 0; i < 10; i++){ 
+        delete queues[i]; 
+    }
+    delete [] queues; //kuyrukalr dizisini de siliyoruz
+    return sirali;
 }
 
 int main() {
-
+    int sayilar[] = {167,32,19,356,478,951,5,98,301,7,};
+    int *sirali = radixSort(sayilar,10);
+    cout<<"Siralamanmadan onceki hali: "<<endl;
+    for (int i = 0; i < 10; i++){
+        cout<<sayilar[i]<<" ";
+    }
+    cout<<endl<<endl<<"Siralandiktan sonraki hali: "<<endl;
+    for (int i = 0; i < 10; i++){
+        cout<<sirali[i]<<" ";
+    }
+    delete [] sirali;
+    return 0;
 }
--- Queue.hpp
+++ Queue.hpp
@@ -7,16 +7,33 @@
 using namespace std;
 
 template <typename Object>
+struct Node{
+    Object item;
+    Node<Object> *next;
+
+    Node(Object item, Node<Object> *next = NULL){ //girilmezse null a aesitle varsayılan parametre
+        this->item = item;
+        this->next = next;
+    }
+};
+
+template <typename Object>
 class Queue{
     private:
-        int front;
+        Node<Object> *front; // veri silinirse harekt edecek 
+        Node<Object> *back; // veri eklenirse harekt edecek 
+        int length; // uzunluk da lazım
+        //dizi üzerinde degil düğüm üzerinde gostercegiz, bunalr artık tam sayı degil adres olacak o yüzden iptal ettik
+        /* int front;
         int back;
         int capacity;
         int length;
         Object *items; //dizinin ilk elemnaını gosteren pointer
         //length kapasiteye esit olmadıgı sürece yer var demektir 
-        //referans noktam uzunluk, uuznluk sıfırsa kuyrukta eleman yoktur
-        void reserve(int newCapacity) {
+        //referans noktam uzunluk, uuznluk sıfırsa kuyrukta eleman yoktur */
+
+        //reserve e de gerek kalmadı dizi gibi dolmaz cunku liste sürekli ekelme yapılabilir
+        /*void reserve(int newCapacity) {
             Object *tmp = new Object[newCapacity];
             for (int j = 0, i = front; j < length; j++){ //kuyruk sıfırdan baslamıyor olabilir, tmp i biz belirledigimiz ici o sıralı fakat i yani items garanti olması acısından front tan baslatmamız gerekiyor
             //j yi birer artıyoruoruz i yi dizinin dısında tasmaması adına modunu alarak arttıracagız
@@ -28,20 +45,28 @@ class Queue{
             items = tmp; // artık items yeni diziyi gosterecek
             front = 0; // artık frontun 0. adres olduguna eminim (tmp de duzenledik yukarıdaki for dongusunde)
             back = length - 1; //front gibi duzenlengigi icin dogal olarak en sondaki adresde
-        }
+        }*/
     public:
-        Queue() { //kurucu method
+        Queue(){
+            front = back = NULL;
+            length = 0;
+        }
+        
+        /*Queue() { //kurucu method
             capacity = 100;
             front = 0;
             back = -1;
             length = 0;
             items = new Object[capacity];
-        }
+        }*/
         void clear() {
+           while(!isEmpty()) dequeue(); // teker teker tüğm dügümler yokedilecek
+        }
+        /*void clear() {
             front = 0;
             back = -1;
             length = 0; //array listte sadece lentghi sıfırlamıstık amac yapıyı baslangıc konumuna getirmek :)
-        }
+        }*/
         int count()const{ //get methodu, sabis bir func.
             return length;
         }
@@ -51,24 +76,47 @@ class Queue{
         //yıgıttaki top func. gibi , cıkmak ğzere olan veriyi gosterecek func.:
         const Object& peek(){ //sadece okuma amaclı bir func.
             if(isEmpty()) throw "queue is empty!";
-            return items[front]; // front indexi neredeyse veri oradadır
+            return front->item;
         }
+        /*const Object& peek(){ //sadece okuma amaclı bir func.
+            if(isEmpty()) throw "queue is empty!";
+            return items[front]; // front indexi neredeyse veri oradadır
+        }*/
         void enqueue(const Object& item) {
+            Node<Object> *last = new Node<Object>(item);
+            if(isEmpty()) front = back = last;
+            else{
+                back->next = last; //last heapte fakat lokal bir pointer func. kapandıgında silinecek 
+                back = last; // kaybolamması icin backi yeniden tanımlkyorum
+            }
+            length++;
+        }
+       /* void enqueue(const Object& item) {
             if(length == capacity) reserve(2*capacity);
             back = (back + 1) % capacity; //bir hata olmaması adına yine mod alarak gidiyorum
             //elemanı ekleemden once kuyrugun sonunu ilerletecegim daha sonrasında eklenecek
             items[back] = item; // back'in icinde oldugu index'e yeni veriyi ekliyorum
             length++; // sonrasında eleman sayısı arttı
-        }
+        }*/
         void dequeue(const Object& item){
+            if(isEmpty()) throw "queue is empty!";
+            Node<Object> *tmp = front;//cop kalmasın diye gecici bir gostericiye atmam lzaım
+            front = fornt->next; // boylece bastaki silindi
+            delete tmp;
+            length--;
+        }
+        /*void dequeue(const Object& item){
             //eklemenin tersi bu sefer back degil front ilerleyecek
             if(isEmpty()) throw "queue is empty!";
             front = (front + 1) % capacity;
             length--;
-        }
+        }*/
         ~Queue(){
-            delete [] items;
+            clear();
         }
+        /*~Queue(){
+            delete [] items;
+        }*/
 };
 
 #endif
