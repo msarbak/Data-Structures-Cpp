5ca6551f014c925f8d9dcda66a2a7231fcf76b66
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-09-12 12:24:37 +0300

    LinkedList

--- LinkedList.cpp
+++ LinkedList.cpp
@@ -1,6 +1,94 @@
 #include "LinkedList.hpp"
+#include <cstdlib>
 
 int main(){
-    
+    LinkedList<string> *names = new LinkedList<string>();
+    int choice = 0;
+    string name;
+    int position;
+    do{//secenekli menu yapmak icin dongu
+        system("CLS"); //her calismada ekran temizligi
+        cout<<"[1]\tAdd Name"<<endl;
+        cout<<"[2]\tRemove Name"<<endl;
+        cout<<"[3]\tRemove From Posiiton"<<endl;
+        cout<<"[4]\tInsert Name"<<endl;
+        cout<<"[5]\tFind Poisiton"<<endl;
+        cout<<"[6]\tNumber of Names"<<endl;
+        cout<<"[7]\tFirst Name"<<endl;
+        cout<<"[8]\tLast Name"<<endl;
+        cout<<"[9]\tList Names"<<endl;
+        cout<<"[10]\tClear Names"<<endl;
+        cout<<"[11]\tExit"<<endl;
+        cin>>choice;
+        system("CLS");
+
+        switch(choice){
+            case 1:
+                cout<<"Name: ";
+                cin.ignore();
+                getline(cin,name); //ikiden fazla kelime girebilir isim icin
+                names->add(name);
+            break;
+            case 2:
+                cout<<"Name: ";
+                cin.ignore();
+                getline(cin,name); //ikiden fazla kelime girebilir isim icin
+                names->remove(name);
+            break;
+            case 3:
+                cout<<"Position: ";
+                cin>>position;
+                names->removeAt(position);
+            break;
+            case 4:
+                cout<<"Position: ";
+                cin>>position;
+                cout<<"Name: ";
+                cin.ignore();
+                getline(cin,name);
+                names->insert(position,name);
+            break;
+            case 5:
+                cout<<"Name: ";
+                cin.ignore();
+                getline(cin,name);
+                cout<<"Position: "<<names->indexOf(name);
+                cin.get(); //ekran dondugu icin sürekli durudurup konumu gorememiz gerekiyor
+            break;
+            case 6:
+                cout<<"Number of names: "<<names->count();
+                cin.ignore();
+                cin.get(); // bazen karaktersonlandırma calıstaıabiliyor onun icin gormezden gel func. ignore var
+            break; 
+            case 7:
+                cout<<"First name: "<<names->first();
+                cin.ignore();
+                cin.get();
+            break;
+            case 8:
+                cout<<"Last name: "<<names->last();
+                cin.ignore();
+                cin.get();
+            break;
+            case 9:
+                cout<<"Names: "<<endl;
+                cout<<*names<<endl;//ekrana yazma operatounu tanımaldıgım icin direkt bu sekilde yazabilirim
+                cin.ignore();
+                cin.get();
+            break;
+            case 10:
+                names->clear();
+            break;
+            case 11:
+            break;
+            default:
+                cout<<endl<<"Wrong choice!!"<<endl<<endl;
+                cin.ignore();
+                cin.get();
+            break;
+        }
+        system("CLS");
+    }while(choice != 11);
+    delete names;
     return 0;
 }
--- LinkedList.hpp
+++ LinkedList.hpp
@@ -21,6 +21,16 @@ class LinkedList{
     private:
         Node<Object> *head; //dugum gosterici
         int size;
+
+        Node<Object>* FindPrevByPosition(int position){ //ekleme silmede bir öncesini döndüren bir adrese ihtiyacımız var bunu kullanıcaz
+            if(position < 0 || position > size) throw "Index out of range!";
+            int index = 1; // bir öncesini bulacagım icin 1 den baslatıyorum 0 dan baslatamam
+            for(Node<Object>* itr = head; itr != NULL; itr = itr->next, index++){ //1. dugumun adresini lokal pointera verdim, ilerleme kaydetmek istiyorum. headin degismemei lazım yoksa listeyi kaybederiz. burada degistirecegimiz sey "itr"
+            // itr = itr->next demek her turda br ilerlemesi demek
+                if(position == index) return itr;
+            }
+            return NULL; //1. dugumunoncesi istendiyse yok ve null doner (gerekirse)
+        }
     public:
         LinkedList(){
             head = NULL; //ilk dugumu gosterecek adres ortada dgum olmadı gı icin null
@@ -38,6 +48,74 @@ class LinkedList{
         }
         const Object& last(){
             if(isEmpty()) throw "Empty list!";
+            return FindPrevByPosition(size)->item; //size aslında son degil ama bir oncesine gittigi icin son olmus oalcak
+        }
+        void add(const Object& item){
+            insert(size,item); //konum size yani sona ekleyecek, neyi ekleyecek item ı
+        }
+        void insert(int index, const Object& item){
+            if(index == 0) head = new Node<Object>(item,head); //head artık yeni eklenen yeri gosteriyor, basa ekleme durumu icin
+            //araya ve sona ekleme isi birlestirilebilir:
+            else{
+                Node<Object> *prev = FindPrevByPosition(index); //bir öncesini bulmak istiyorum
+                //bulduktan sonra prev olarak gosterdigim dügümün nexti olarak araya eleman eklerim, onunda nexti previn nexti olur 
+                prev->next = new Node<Object>(item,prev->next);
+                //burada ilk parametre dugumun verisiydi dolayısıyla item yani yeni istedigimiz veri bu, ikinci parametre next adresiydi dugum yapsıına bakarsak, burada da previn nexti yeni eklenecek dugumun nexti oldu 
+                //sornasında da previmnn kendisi lokal pointer oldugu icin silinir fakat gosterdigib heap bellek bolgesinde oldugu icin  silinmeyecektir
+            }
+            size++; //her kosulda eleman sayisi artacak
+        }
+        void remove(const Object& item){ // su veriyi sil func.
+            int index = indexOf(item);
+            removeAt(index);
+        }
+        void removeAt(int index){ //konumdakini sil func.
+            if(size == 0) throw "Empty list!";
+            Node<Object> *del; //sielcegim dugumu gosterecek bir lokal pointer tanımlıyorum. çöp olusmamıs icin en son bunun gosterdigini delete diyecegim
+            if(index == 0){//ilk elemanı silme durumu
+                del = head; //silecegim kısmı dele atıyorum çöp olmasın diye
+                head = head->next; //headi bir ilerletiorum
+            } else{// aradan veya sondan eleman silme
+                Node<Object> *prev = FindPrevByPosition(index);
+                del = prev->next;
+                prev->next = prev->next->next;
+            }
+            delete del;
+            size--;
+        }
+        int indexOf(const Object& item){ // verinin adresini getirecek
+            int index = 0;
+            for(Node<Object> *itr = head; itr != NULL; itr->next,index++){ //listenin basından baslayıp hepsini gezecegim bir dongu   
+                if(itr->item == item) return index;
+            }
+            throw "Index out of range!";
+        }
+        bool find(const Object& item){ // su veri var mı?
+            for(Node<Object> *itr = head; itr != NULL; itr = itr->next){ 
+                if(itr->item == item) return true;
+            }
+            return false;
+        }
+        friend ostream& operator<<(ostream& screen, LinkedList<Object>& right) {
+            if(right.isEmpty()){
+                screen<<"Empty List!"<<endl;
+            } else {
+                for(Node<Object> *itr = head; itr != NULL; itr = itr->next){ 
+                screen<<itr->item<<" ";
+                }
+            }
+            return screen;
+        }
+        void clear() {
+            /*head = NULL;
+            size = 0; 
+            // bu sekilde baslangic konumuna doner ancak butun dugumler cop olarak kalacaktır */
+            while(!isEmpty()){ //liste dolu oldugu sürece
+                removeAt(0); //her turda en bastakini sil
+            }
+        }
+        ~LinkedList(){
+            clear();
         }
 };
 
