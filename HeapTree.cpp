7b3d7f61145a72f1416459daed367aee0f11a622
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-09-14 21:23:04 +0300

    HeapTree

---
+++ HeapTree.cpp
@@ -0,0 +1,7 @@
+#include "HeapTree.hpp"
+
+
+int main(){ 
+
+    return 0;
+}
---
+++ HeapTree.hpp
@@ -0,0 +1,12 @@
+#ifndef HEAPTREE_HPP
+#define HEAPTREE_HPP
+
+#include <iostream>
+using namespace std;
+
+template <typename Object>
+struct Node{
+
+};
+
+#endif
