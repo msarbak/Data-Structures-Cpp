c2ea52b2f7d6c86f8608e7bfa3fd0af7a539438b
Mehmet ŞARBAK <m.sarbak@hotmail.com>
2022-09-19 14:04:30 +0300

    AVL

---
+++ AVL.cpp
@@ -0,0 +1,6 @@
+#include "AVL.hpp"
+
+int main(){
+    
+    return 0;
+}
---
+++ AVL.hpp
@@ -0,0 +1,13 @@
+#ifndef AVL_HPP
+#define AVL_HPP
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+#define SIZE 1000
+
+template <typename Object>
+class AVL{
+};
+#endif;
